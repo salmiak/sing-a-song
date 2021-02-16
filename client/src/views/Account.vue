750e1e155a6b912b7f290a441b525d75d7ff56c0
Alfred Beckman <alfred@salmiakmedia.se>
2021-02-16 23:07:15 +0100

    Adding tabs to account page

--- client/src/views/Account.vue
+++ client/src/views/Account.vue
@@ -7,56 +7,84 @@
     </v-row>
     <v-row>
       <v-col>
-        <h2>Ditt konto</h2>
-        <p>Gick med {{user.created}}</p>
-
-        <h3>Kontoinformation</h3>
-        <v-form @submit.prevent="updateAccount">
-          <v-text-field
-            v-model="user.firstName"
-            label="Förnamn" ></v-text-field>
-          <v-text-field
-            v-model="user.lastName"
-            label="Efternamn" ></v-text-field>
-          <v-text-field
-            v-model="user.email"
-            label="Email" ></v-text-field>
-          <v-btn
-            @click="updateAccount"
-            :disabled="submitted">Uppdatera konto</v-btn>
-        </v-form>
-
-        <h3>Ändra lösenord</h3>
-        <v-form>
-          <v-text-field
-            v-model="password"
-            label="Lösenord"
-            type="password" ></v-text-field>
-          <v-text-field
-            v-model="confirmPassword"
-            label="Lösenord igen"
-            type="password" ></v-text-field>
-          <v-btn
-            @click="updatePassword"
-            :disabled="submitted">Uppdatera lösenord</v-btn>
-        </v-form>
-      </v-col>
-      <v-col>
-        <h2>Din profil</h2>
-        <template v-if="profile">
-          <p>Beskrivning: {{profile.description}}</p>
-        </template>
-        <template v-else>
-          <p>Laddar profil...</p>
-        </template>
+        <v-tabs
+          v-model="tabs"
+          color="red"
+        >
+          <v-tab>Ditt konto</v-tab>
+          <v-tab>Din profil</v-tab>
+        </v-tabs>
       </v-col>
     </v-row>
+
+    <v-tabs-items v-model="tabs">
+
+      <v-tab-item>
+        <v-row>
+          <v-col class="col-6">
+            <h2>Ditt konto</h2>
+            <p>Gick med {{user.created}}</p>
+
+            <h3>Kontoinformation</h3>
+            <v-form @submit.prevent="updateAccount">
+              <v-text-field
+                v-model="user.firstName"
+                label="Förnamn" ></v-text-field>
+              <v-text-field
+                v-model="user.lastName"
+                label="Efternamn" ></v-text-field>
+              <v-text-field
+                v-model="user.email"
+                label="Email" ></v-text-field>
+              <v-btn
+                @click="updateAccount"
+                :disabled="submitted">Uppdatera konto</v-btn>
+            </v-form>
+
+            <h3>Ändra lösenord</h3>
+            <v-form>
+              <v-text-field
+                v-model="password"
+                label="Lösenord"
+                type="password" ></v-text-field>
+              <v-text-field
+                v-model="confirmPassword"
+                label="Lösenord igen"
+                type="password" ></v-text-field>
+              <v-btn
+                @click="updatePassword"
+                :disabled="submitted">Uppdatera lösenord</v-btn>
+            </v-form>
+          </v-col>
+        </v-row>
+      </v-tab-item>
+
+      <v-tab-item>
+        <v-row>
+          <v-col>
+            <h2>Din profil</h2>
+            <template v-if="profile">
+              <p>Beskrivning: {{profile.description}}</p>
+            </template>
+            <template v-else>
+              <p>Laddar profil...</p>
+            </template>
+          </v-col>
+        </v-row>
+      </v-tab-item>
+
+    </v-tabs-items>
   </v-container>
 </template>
 
 <script>
 export default {
   name:'Account',
+  data() {
+    return {
+      tabs: null
+    }
+  },
   computed: {
     user: {
       get() {
