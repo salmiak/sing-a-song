2d7d4bede3a10387e77300dd9fe470c50f849777
Alfred Beckman <alfred@salmiakmedia.se>
2021-02-22 23:39:54 +0100

    Update send-email.js

--- _helpers/send-email.js
+++ _helpers/send-email.js
@@ -3,8 +3,17 @@ const nodemailer = require('nodemailer');
 
 module.exports = sendEmail;
 
+const smtpOptions = {
+  "host": "smtp.ethereal.email",
+  "port": 587,
+  "auth": {
+    "user": process.env.smtp_user || require('config.json').smtpUser,
+    "pass": process.env.smtp_pass || require('config.json').smtpPass
+  }
+}
+
 async function sendEmail({ to, subject, html, from = "localhost@salmiakmedia.se" }) {
-    // const transporter = nodemailer.createTransport(config.smtpOptions);
-    // await transporter.sendMail({ from, to, subject, html });
-    console.log({to, subject, html}) // eslint-disable-line no-console
+    const transporter = nodemailer.createTransport(smtpOptions);
+    await transporter.sendMail({ from, to, subject, html });
+    // console.log({to, subject, html}) // eslint-disable-line no-console
 }
