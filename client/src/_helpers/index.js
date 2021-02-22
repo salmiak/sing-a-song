d768ce913e0a3ee89b5d6009ee655b4f765753ea
Alfred Beckman <alfred@salmiakmedia.se>
2021-02-22 22:55:27 +0100

    update apiURL based on env.

--- client/src/_helpers/index.js
+++ client/src/_helpers/index.js
@@ -1,6 +1,6 @@
 import { authHeader } from './auth-header';
 
-const apiUrl = 'http://localhost:4000'
+const apiUrl = process.env.NODE_ENV === 'production' ? '/' : 'http://localhost:4000'
 
 export default {
   authHeader,
